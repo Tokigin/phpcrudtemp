6f2d2973b6a7b25d981d605b8d4ca39696160beb
Tokigin <tokigin321@gmail.com>
2024-03-22 15:55:18 +0630

    cmt

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "script/script.php";
 ?>
 <div class="p-3 text-center container-fluid">
     <h1 class="pb-2 border-bottom">Latest Product</h1>
-    <p class="text-sm font-weight-normal">Our Products are here!</p>
+    <p class="text-sm font-weight-normal">Our Products are here! XD</p>
 </div>
 
 <div class="row row-cols-1 row-cols-md-3 g-5 p-5 container-fluid">
